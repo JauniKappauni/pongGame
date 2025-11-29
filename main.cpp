a81d68e357113f5e6e2e583bbcc2434f3afe4c78
JauniKappauni <social.jauni@gmail.com>
2025-11-29 19:14:35 +0100

    feat: add player movement

--- main.cpp
+++ main.cpp
@@ -1,13 +1,42 @@
 #include <raylib.h>
 
+float speed = 100.0f;
+
+float playerOnePosY = 0;
+float playerTwoPosY = 0;
+
+int screenWidth = 800;
+int screenHeight = 400;
+
 int main()
 {
-    InitWindow(800, 450, "Pong Game");
+    InitWindow(screenWidth, screenHeight, "Pong Game");
 
     while (!WindowShouldClose())
     {
+        float dt = GetFrameTime();
         BeginDrawing();
         ClearBackground(BLACK);
+        DrawLine(10, playerOnePosY, 10, playerOnePosY + 100, WHITE);
+        DrawLine(790, playerTwoPosY, 790, playerTwoPosY + 100, WHITE);
+
+        if (IsKeyDown(KEY_W) && playerOnePosY > 0)
+        {
+            playerOnePosY -= speed * dt;
+        }
+        if (IsKeyDown(KEY_S) && playerOnePosY + 100 < screenHeight)
+        {
+            playerOnePosY += speed * dt;
+        }
+        if (IsKeyDown(KEY_UP) && playerTwoPosY > 0)
+        {
+            playerTwoPosY -= speed * dt;
+        }
+        if (IsKeyDown(KEY_DOWN) && playerTwoPosY + 100 < screenHeight)
+        {
+            playerTwoPosY += speed * dt;
+        }
+
         EndDrawing();
     }
 
